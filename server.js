cd24b034bc9ed0700c311da5bdced160bec13703
bdavs3 <bdavs3000@gmail.com>
2020-09-23 16:11:34 -0700

    Add initial Spotify integration.
    - Retrieve access token to query API for basic user info, as outlined in
      the 'authorization_code' portion of the Spotify API docs.

---
+++ server.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const querystring = require('querystring');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const got = require('got');
+
+const CLIENT_ID = 'f50a09d5921542feb41008ac70af146c';
+const CLIENT_SECRET = 'db75b49b56df42c4b3adc6dd36242d36';
+const REDIRECT_URI = 'http://localhost:8888/callback';
+const SCOPE = 'playlist-modify-public playlist-modify-private';
+const STATE_KEY = 'spotify-auth-state';
+
+class Server {
+  static generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+  authorize() {
+    let app = express();
+
+    app.use(express.static(__dirname + '/public'))
+      .use(cors())
+      .use(cookieParser());
+
+    app.get('/login', (req, res) => {
+      let state = Server.generateRandomString(16);
+      res.cookie(STATE_KEY, state);
+
+      res.redirect('https://accounts.spotify.com/authorize?' + 
+        querystring.stringify({
+          response_type: 'code',
+          client_id: CLIENT_ID,
+          scope: SCOPE,
+          redirect_uri: REDIRECT_URI,
+          state: state
+        }));
+    });
+
+    app.get('/callback', (req, res) => {
+      let code = req.query.code || null;
+      let state = req.query.state || null;
+      let storedState = req.cookies ? req.cookies[STATE_KEY] : null;
+
+      if (state === null || state !== storedState) {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'state_mismatch'
+          }));
+      } else {
+        res.clearCookie(STATE_KEY);
+        let authOptions = {
+          url: 'https://accounts.spotify.com/api/token',
+          form: {
+            code: code,
+            redirect_uri: REDIRECT_URI,
+            grant_type: 'authorization_code'
+          },
+          headers: {
+            'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+          },
+          json: true
+        };
+
+        (async () => {
+          try {
+            const response = await got.post(
+              authOptions.url, 
+              { 
+                form: authOptions.form, 
+                headers: { 'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')) } 
+              }
+            );
+            
+            let body = JSON.parse(response.body);
+            let access_token = body.access_token,
+                refresh_token = body.refresh_token;
+
+            console.log(access_token);
+
+            let options = {
+              url: 'https://api.spotify.com/v1/me',
+              headers: { 'Authorization': 'Bearer ' + access_token }
+            };
+
+            (async () => {
+              try {
+                const response = await got.get(options.url, { headers: options.headers });
+                console.log(response.body);
+              } catch (error) {
+                console.log('Error in get:');
+                console.log(error.message);
+              }
+            })();
+
+            res.redirect('/#' +
+              querystring.stringify({
+                access_token: access_token,
+                refresh_token: refresh_token
+              }));
+          } catch (error) {
+            console.log('Err in post:');
+            console.log(error.message);
+          }
+        })();
+      }
+    });
+
+    app.listen(8888, () => {
+      console.log("Listening on 8888...");
+    });
+  }
+}
+
+module.exports = Server;
